f7f615567f9e33578f14ed5babf70b0f5bbb0942
Ryan O. Schenck <ryanschenck@mail.usf.edu>
2017-11-23 14:46:38 +0000

    all data present, and script completed

--- MainDataParsing.py
+++ MainDataParsing.py
@@ -1,6 +1,7 @@
 import sys
 import glob
 import numpy as np
+from scipy import stats
 
 DataFilePath = "./Data"
 
@@ -30,29 +31,57 @@ def DataParserAmp(rawData, n):
 
 def Grouper(DataObj):
     CellTypes = list(set([myData.cellLine for myData in DataObj]))
+    # print(CellTypes)
     Output = {item:[] for item in CellTypes}
     for cell in DataObj:
         Output[cell.cellLine].append(np.average([cell.tau_data["tau_amp"], cell.tau_data["tau_int"]]))
 
-    print("Cell\tAverage\tStd")
+    print("\nCell\tAverage\tStd")
     for item in Output:
         print("%s\t%s\t%s"%(item,np.average(Output[item]),np.std(Output[item])))
 
-def writePhotons(DataObj):
-    with open("PhotonOutput.txt",'w') as outputFile:
+    Output = {"SI_BS":[],"WT_BS":[],"WT_BS_10uM_ola_cath":[],"SI_BS_10uM_ola_cath":[],"SI_BS_H2O2_cath":[], "WT_BS_H2O2_cath":[]}
+    print("\nROI\ttau")
+    with open("tau_distribution.txt", "w") as outTau:
         for cell in DataObj:
-            for line in cell.dataOut:
-                if line.startswith("Time[ns]"):
-                    pass
-                else:
-                    outputFile.write(cell.filename.rstrip('.dat') + '\t' + line + '\n')
+            if "BS" in cell.cellLine:
+                print("%s\t%s"%(cell.cellLine, np.average([cell.tau_data["tau_amp"], cell.tau_data["tau_int"]])))
+                outTau.write(cell.cellLine + "\t" + repr(np.average([cell.tau_data["tau_amp"], cell.tau_data["tau_int"]])) + "\n")
+                Output[cell.cellLine].append(np.average([cell.tau_data["tau_amp"], cell.tau_data["tau_int"]]))
+
+    print("\ntau t-tests...")
+    print("SI_BS vs. WT_BS")
+    print(stats.ttest_ind(Output["SI_BS"],Output["WT_BS"]))
+    print("SI_BS_10uM_ola_cath vs. WT_BS_10uM_ola_cath")
+    print(stats.ttest_ind(Output["SI_BS_10uM_ola_cath"], Output["WT_BS_10uM_ola_cath"]))
+    print("SI_BS_H2O2_cath vs. WT_BS_H2O2_cath")
+    print(stats.ttest_ind(Output["SI_BS_H2O2_cath"], Output["WT_BS_H2O2_cath"]))
+
+    Output = {"SI_BS": [], "WT_BS": [], "WT_BS_10uM_ola_cath": [], "SI_BS_10uM_ola_cath": [], "SI_BS_H2O2_cath": [], "WT_BS_H2O2_cath":[]}
+    print("\nROI\tfc")
+    with open("fc_distribution.txt","w") as outFc:
+        for cell in DataObj:
+            if "BS" in cell.cellLine:
+                fc = cell.parameters["Ampl.1"] / (cell.parameters["Ampl.1"] + cell.parameters["Ampl.2"])
+                print("%s\t%s"%(cell.cellLine, fc))
+                outFc.write(cell.cellLine+"\t"+repr(fc)+"\n")
+                Output[cell.cellLine].append(fc)
+
+    print("\nfc t-tests...")
+    print("SI_BS vs. WT_BS")
+    print(stats.ttest_ind(Output["SI_BS"], Output["WT_BS"]))
+    print("SI_BS_10uM_ola_cath vs. WT_BS_10uM_ola_cath")
+    print(stats.ttest_ind(Output["SI_BS_10uM_ola_cath"], Output["WT_BS_10uM_ola_cath"]))
+    print("SI_BS_H2O2_cath vs. WT_BS_H2O2_cath")
+    print(stats.ttest_ind(Output["SI_BS_H2O2_cath"], Output["WT_BS_H2O2_cath"]))
 
 class Data:
 
     def __init__(self, file):
         self.filename = file
         self.rawData = DataReader(self.filename)
-        self.cellLine = "_".join(file.split("/")[len(file.split("/"))-1].split("_")[0:4])
+        self.cellLine = file.split("/")[len(file.split("/"))-1].split("_cell")[0]
+        print(self.cellLine)
         if "Ampl. 2" in ','.join(self.rawData):
             self.n = 2
             self.parameters, self.chiSquare, self.tau_data, self.dataOut = DataParserAmp(self.rawData, self.n)
@@ -69,4 +98,4 @@ if __name__=="__main__":
 
     Grouper(ROIs)
 
-    writePhotons(ROIs)
+    # writePhotons(ROIs)
